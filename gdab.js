21f49ca955593351ef0c2811d801b7f752143e10
q8j. <156719351+q8j-dev@users.noreply.github.com>
2025-12-26 13:41:18 -0600

    Update gdab.js

--- gdab.js
+++ gdab.js
@@ -4,7 +4,6 @@ window.onbeforeunload = function() {
   }, 1);
 };
 
-const originalWindowOpen = window.open;
 window.open = function() {
   return null;
 };
@@ -13,155 +12,131 @@ window.confirm = function() {
   return false;
 };
 
-window.alert = function(message) {
-};
+window.alert = function() {};
 
-const currentDomain = window.location.hostname;
+const currentOrigin = window.location.origin;
 
-function isSameDomain(url) {
+function isAllowed(url) {
   if (!url) return true;
+  if (/^(data|blob|about):/.test(url)) return true;
   try {
-    if (url.startsWith('/') || !url.includes('://')) {
-      return true;
+    const parsed = new URL(url, currentOrigin);
+    const hostname = parsed.hostname;
+    if (hostname === 'gamedistribution.com' || hostname.endsWith('.gamedistribution.com')) {
+      return false;
     }
-   
-    const urlObj = new URL(url);
-    return urlObj.hostname === currentDomain;
+    return true;
   } catch (e) {
-    return false;
+    return true;
   }
 }
 
 const originalFetch = window.fetch;
 window.fetch = function(resource, options) {
-  const url = typeof resource === 'string' ? resource : resource.url;
-   
-  if (isSameDomain(url)) {
+  const url = resource instanceof Request ? resource.url : resource;
+  if (isAllowed(url)) {
     return originalFetch.apply(this, arguments);
-  } else {
-    console.log("BLOCKED REQUEST:", url);
-    return new Promise((resolve, reject) => {
-      reject(new Error("REQUEST BLOCKED"));
-    });
   }
+  console.error("BLOCKED REQUEST:", url);
+  return Promise.reject(new Error("REQUEST BLOCKED"));
 };
 
 const originalXHR = window.XMLHttpRequest;
 window.XMLHttpRequest = function() {
   const xhr = new originalXHR();
   const originalOpen = xhr.open;
-   
-  xhr.open = function(method, url, ...rest) {
-    xhr._blockedUrl = !isSameDomain(url) ? url : null;
-    return originalOpen.apply(this, [method, url, ...rest]);
-  };
-   
   const originalSend = xhr.send;
+
+  xhr.open = function(method, url) {
+    xhr._blockedUrl = !isAllowed(url) ? url : null;
+    return originalOpen.apply(this, arguments);
+  };
+
   xhr.send = function(body) {
     if (xhr._blockedUrl) {
-      console.log("BLOCKED REQUEST:", xhr._blockedUrl);
-      Object.defineProperty(this, 'status', { value: 0 });
-      Object.defineProperty(this, 'statusText', { value: 'Error' });
-       
-      setTimeout(() => {
-        const errorEvent = new Event('error');
-        this.dispatchEvent(errorEvent);
+      console.error("BLOCKED REQUEST:", xhr._blockedUrl);
+      Object.defineProperty(xhr, 'status', { value: 0, writable: true });
+      Object.defineProperty(xhr, 'statusText', { value: 'Error', writable: true });
+      Object.defineProperty(xhr, 'readyState', { value: 4, writable: true });
+      
+      setTimeout(function() {
+        xhr.dispatchEvent(new Event('error'));
+        xhr.dispatchEvent(new Event('readystatechange'));
       }, 0);
-       
       return;
     }
     return originalSend.apply(this, arguments);
   };
-   
+
   return xhr;
 };
 
-const originalImage = window.Image;
-window.Image = function() {
-  const img = new originalImage();
-  const originalSrcDescriptor = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src');
-   
-  Object.defineProperty(img, 'src', {
-    set: function(value) {
-      if (isSameDomain(value)) {
-        originalSrcDescriptor.set.call(this, value);
-      } else {
-        console.log("BLOCKED IMAGE:", value);
-      }
-    },
+function shimSrc(target, Prototype, logMessage) {
+  const descriptor = Object.getOwnPropertyDescriptor(Prototype.prototype, 'src');
+  Object.defineProperty(target, 'src', {
+    configurable: true,
+    enumerable: true,
     get: function() {
-      return originalSrcDescriptor.get.call(this);
+      return descriptor.get.call(this);
+    },
+    set: function(value) {
+      if (isAllowed(value)) {
+        descriptor.set.call(this, value);
+      } else {
+        console.error(logMessage, value);
+      }
     }
   });
-   
+}
+
+const originalImage = window.Image;
+window.Image = function() {
+  const img = new originalImage(...arguments);
+  shimSrc(img, HTMLImageElement, "BLOCKED IMAGE:");
   return img;
 };
 
 const originalCreateElement = document.createElement;
 document.createElement = function(tagName) {
-  const element = originalCreateElement.call(document, tagName);
-   
-  if (tagName.toLowerCase() === 'script') {
-    const originalSrcDescriptor = Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, 'src');
-     
-    Object.defineProperty(element, 'src', {
-      set: function(value) {
-        if (isSameDomain(value)) {
-          originalSrcDescriptor.set.call(this, value);
-        } else {
-          console.log("BLOCKED SCRIPT:", value);
-        }
-      },
-      get: function() {
-        return originalSrcDescriptor.get.call(this);
-      }
-    });
+  const element = originalCreateElement.apply(document, arguments);
+  const tag = tagName.toLowerCase();
+  
+  if (tag === 'script') {
+    shimSrc(element, HTMLScriptElement, "BLOCKED SCRIPT:");
+  } else if (tag === 'iframe') {
+    shimSrc(element, HTMLIFrameElement, "BLOCKED IFRAME:");
+  } else if (tag === 'img') {
+    shimSrc(element, HTMLImageElement, "BLOCKED IMAGE:");
   }
-   
-  if (tagName.toLowerCase() === 'iframe') {
-    const originalSrcDescriptor = Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, 'src');
-     
-    Object.defineProperty(element, 'src', {
-      set: function(value) {
-        if (isSameDomain(value)) {
-          originalSrcDescriptor.set.call(this, value);
-        } else {
-          console.log("BLOCKED IFRAME:", value);
-        }
-      },
-      get: function() {
-        return originalSrcDescriptor.get.call(this);
-      }
-    });
-  }
-   
+  
   return element;
 };
 
 const originalSendBeacon = navigator.sendBeacon;
 navigator.sendBeacon = function(url, data) {
-  if (isSameDomain(url)) {
-    return originalSendBeacon.call(this, url, data);
-  } else {
-    console.log("BLOCKED REQUEST:", url);
-    return false;
+  if (isAllowed(url)) {
+    return originalSendBeacon.apply(this, arguments);
   }
+  console.error("BLOCKED REQUEST:", url);
+  return false;
 };
 
 const originalWebSocket = window.WebSocket;
 window.WebSocket = function(url, protocols) {
-  if (isSameDomain(url)) {
+  if (isAllowed(url)) {
     return new originalWebSocket(url, protocols);
-  } else {
-    console.log("BLOCKED CONNECTION:", url);
-    return {
-      send: function() {},
-      close: function() {},
-      addEventListener: function() {}
-    };
   }
+  console.error("Blocked connection:", url);
+  return {
+    url: url,
+    readyState: 3,
+    send: function() {},
+    close: function() {},
+    addEventListener: function() {},
+    removeEventListener: function() {},
+    dispatchEvent: function() { return false; }
+  };
 };
 
 console.log("GDAB is running!");
-
 console.log("GDAB, by syncintellect @ github, and maintained by q8j-dev in conjuction with endlessguyin @ github!");
